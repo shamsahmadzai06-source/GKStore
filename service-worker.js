df2f883680b4eaf74615fe41a20e8d8b6ee854ef
shamsahmadzai06-source <shamsahmadzai06@gmail.com>
2026-01-26 17:38:21 +0430

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,21 +13,31 @@ const FILES = [
   "./android-launchericon-512-512.png"
 ];
 
+// Install Event
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
+// Activate Event
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
     )
   );
   self.clients.claim();
 });
 
+// Fetch Event
 self.addEventListener("fetch", e => {
   if (e.request.method !== "GET") return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
