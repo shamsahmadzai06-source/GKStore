eeabd9f7b4ef026dd6e810c6d9283b5f16f12edd
shamsahmadzai06-source <shamsahmadzai06@gmail.com>
2026-03-25 22:54:13 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -38,9 +38,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
+      if (response) return response;
       return fetch(event.request);
     })
   );
