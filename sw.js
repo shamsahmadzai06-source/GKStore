7fcab5c7909e04be3bfe8d6f57000c929c97d25b
shamsahmadzai06-source <shamsahmadzai06@gmail.com>
2026-04-21 15:33:15 +0430

    Update service worker cache version to v4

--- sw.js
+++ sw.js
@@ -1,24 +1,11 @@
-const CACHE_NAME = 'gk-store-v3';
+const CACHE_NAME = 'gk-store-v4';
 const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json',
-  'android-launchericon-72-72.png',
-  'android-launchericon-96-96.png',
-  'android-launchericon-144-144.png',
-  'android-launchericon-192-192.png',
+  '.', 'index.html', 'manifest.json',
+  'android-launchericon-72-72.png', 'android-launchericon-96-96.png',
+  'android-launchericon-144-144.png', 'android-launchericon-192-192.png',
   'android-launchericon-512-512.png'
 ];
 
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))));
-});
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key))))); });
