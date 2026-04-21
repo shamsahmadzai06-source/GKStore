e2e675e92d800b6b1a10f3ad62c1f6bc0df0fb4b
shamsahmadzai06-source <shamsahmadzai06@gmail.com>
2026-04-21 11:56:46 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gk-store-v15';
+const CACHE_NAME = 'gk-store-v16';
 const urlsToCache = [
   '.', 'index.html', 'manifest.json', 'backr.png',
   'android-launchericon-72-72.png', 'android-launchericon-96-96.png',
