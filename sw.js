eb18736443fad37a4b384013d81d1a027acc37d4
shamsahmadzai06-source <shamsahmadzai06@gmail.com>
2026-04-21 21:41:54 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gk-store-v8';
+const CACHE_NAME = 'gk-store-v9';
 const urlsToCache = [
   '.', 'index.html', 'manifest.json',
   'android-launchericon-72-72.png', 'android-launchericon-96-96.png',
